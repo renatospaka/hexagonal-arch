c9ab6b01168a917ef919fb67df98ded4a5273531
Renato Spakauskas <renatospaka@gmail.com>
2022-11-12 16:41:15 -0300

    feat: :sparkles: introducing currency
    converting currency when calculating total amount

--- invoice/src/main.ts
+++ invoice/src/main.ts
@@ -1,13 +1,32 @@
+import axios from "axios";
 import express from "express";
 import pgp from "pg-promise";
 
 const app = express();
 app.get("/cards/:cardNumber/invoices", async function (req, res) {
+  const today = new Date();
+  const month = today.getMonth() + 1;
+  const year = today.getFullYear();
+
+  const response = await axios.get("http://app:3001/currencies");
+  const currencies = response.data;
+  console.log(currencies);
+
   const connection = pgp()("postgres://app:secret@db:5432/hexagonal_arch");
-  const transactions = await connection.query("select * from public.card_transaction where card_number = $1", req.params.cardNumber);
+  const transactions = await connection.query(
+    "select * from public.card_transaction where card_number = $1 and extract(month from date) = $2 and extract(year from date) = $3", 
+    [req.params.cardNumber,
+    month,
+    year]
+  );
   let total = 0;
   for (const transaction of transactions) {
-    total += parseFloat(transaction.amount);
+    if (transaction.currency === "BRL") {
+      total += parseFloat(transaction.amount);
+    }
+    if (transaction.currency === "USD") {
+      total += parseFloat(transaction.amount) * currencies.usd;
+    }
   }
   res.json({ 
     total: total 
--- invoice/test/main.test.ts
+++ invoice/test/main.test.ts
@@ -3,5 +3,5 @@ import axios from "axios";
 test("Deve testar a API", async () => {
   const response = await axios.get("http://app:3000/cards/1234/invoices");
   const output = response.data;
-  expect(output.total).toBe(1500);
+  expect(output.total).toBe(1050);
 });
